5919948936f14d1324ed6076dcd6fca77db90eff
zim <zimtsui@gmail.com>
2022-06-12 17:21:41 +0800

    dep

--- build/interfaces.d.ts
+++ build/interfaces.d.ts
@@ -1,7 +1,5 @@
-declare type Defined = number | string | symbol | object | null | bigint;
 export declare type Id = symbol;
-export declare type Dep = Defined;
+export declare type Dep = unknown;
 export declare type Factory<T extends Dep> = () => T;
 export declare type Ctor<T extends Host> = new (...params: any[]) => T;
 export declare type Host = object;
-export {};
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,6 +1,5 @@
-type Defined = number | string | symbol | object | null | bigint;
 export type Id = symbol;
-export type Dep = Defined;
+export type Dep = unknown;
 export type Factory<T extends Dep> = () => T;
 export type Ctor<T extends Host> = new (...params: any[]) => T;
 export type Host = object;
