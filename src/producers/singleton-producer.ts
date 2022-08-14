a8d181560b12f5af5decc13a8b8f78f4c944d6c6
Zim <zimtsui@gmail.com>
2022-08-15 02:05:26 +0800

    singleton lock

--- src/producers/singleton-producer.ts
+++ src/producers/singleton-producer.ts
@@ -21,8 +21,8 @@ export abstract class SingletonProducer<T extends Dep> implements ProducerLike<T
 		if (!this.produced) {
 			assert(!this.locked, new CircularConstructorInjection());
 			this.locked = true;
-			this.produced = true;
 			this.singleton = this.producer.getInstanceWithoutSetterInjection();
+			this.produced = true;
 			this.locked = false;
 			this.producer.setterInject(this.singleton);
 		}
